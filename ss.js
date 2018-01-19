e1f1aeaa50da0d2951ff599ce454be722f909e5b
shqipstream <35532932+shqipstream@users.noreply.github.com>
2018-01-19 04:46:30 +0100

    Add files via upload

---
+++ ss.js
@@ -0,0 +1,151 @@
+$(function(){
+	$('#tch, #tch-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/tch.html" allowfullscreen="1"></iframe>');
+                
+    });
+    $('#klan, #klan-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/klan.html" allowfullscreen="1"></iframe>');
+    });
+    $('#klanplus, #klanplus-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/klanplus.html" allowfullscreen="1"></iframe>');
+    });
+    $('#topnews, #topnews-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/topnews.html" allowfullscreen="1"></iframe>');
+    });
+    $('#abcnews, #abcnews-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/abcnews.html" allowfullscreen="1"></iframe>');
+    });
+
+    $('#news24, #news24-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/news24.html" allowfullscreen="1"></iframe>');
+    });
+    $('#oranews, #oranews-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/oranews.html" allowfullscreen="1"></iframe>');
+    });
+    $('#vizionplus, #vizionplus-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/vizionplus.html" allowfullscreen="1"></iframe>');
+    });
+    $('#reporttv, #reporttv-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/reporttv.html" allowfullscreen="1"></iframe>');
+    });
+    $('#rtsh, #rtsh-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/rtsh.html" allowfullscreen="1"></iframe>');
+    });
+    $('#scan, #scan-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/scan.html" allowfullscreen="1"></iframe>');
+    });
+    $('#syrivision, #syrivision-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/syrivision.html" allowfullscreen="1"></iframe>');
+    });
+    $('#ktv, #ktv-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/ktv.html" allowfullscreen="1"></iframe>');
+    });
+    $('#21rtv, #21rtv-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/21rtv.html" allowfullscreen="1"></iframe>');
+    });
+    $('#rtk, #rtk-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/rtk.html" allowfullscreen="1"></iframe>');
+    });
+    $('#klankosova, #klankosova-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/klankosova.html" allowfullscreen="1"></iframe>');
+    });
+    $('#arta, #arta-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/arta.html" allowfullscreen="1"></iframe>');
+    });
+    $('#alsatm, #alsatm-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/alsatm.html" allowfullscreen="1"></iframe>');
+    });
+    $('#folkplus, #folkplus-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/folkplus.html" allowfullscreen="1"></iframe>');
+    });
+    $('#bbfmusic, #bbfmusic-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/bbfmusic.html" allowfullscreen="1"></iframe>');
+    });
+    $('#ss1, #ss1-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/ss1.html" allowfullscreen="1"></iframe>');
+    });
+    $('#ss2, #ss2-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/ss2.html" allowfullscreen="1"></iframe>');
+    });
+    $('#ss3, #ss3-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/ss3.html" allowfullscreen="1"></iframe>');
+    });
+    $('#ss4, #ss4-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/ss4.html" allowfullscreen="1"></iframe>');
+    });
+    $('#tringsport, #tringsport-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/tringsport.html" allowfullscreen="1"></iframe>');
+    });
+    $('#intv, #intv-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/intv.html" allowfullscreen="1"></iframe>');
+    });
+    $('#expshkence, #expshkence-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/expshkence.html" allowfullscreen="1"></iframe>');
+    });
+    $('#exphistori, #exphistori-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/exphistori.html" allowfullscreen="1"></iframe>');
+    });
+    $('#expnatyre, #expnatyre-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/expnatyre.html" allowfullscreen="1"></iframe>');
+    });
+    $('#trinplanet, #trinplanet-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/trinplanet.html" allowfullscreen="1"></iframe>');
+    });
+    $('#filmaksion, #filmaksion-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/filmaksion.html" allowfullscreen="1"></iframe>');
+    });
+    $('#filmdrame, #filmdrame-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/filmdrame.html" allowfullscreen="1"></iframe>');
+    });
+    $('#filmhits, #filmhits-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/filmhits.html" allowfullscreen="1"></iframe>');
+    });
+    $('#filmnjehd, #filmdyhd-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/filmnjehd.html" allowfullscreen="1"></iframe>');
+    });
+    $('#filmdyhd, #filmdyhd-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/filmdyhd.html" allowfullscreen="1"></iframe>');
+    });
+    $('#bangbang, #bangbang-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/bangbang.html" allowfullscreen="1"></iframe>');
+    });
+    $('#cufo, #cufo-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/cufo.html" allowfullscreen="1"></iframe>');
+    });
+    $('#tringtring, #tringtring-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/tringtring.html" allowfullscreen="1"></iframe>');
+    });
+    $('#juniortv, #juniortv-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/juniortv.html" allowfullscreen="1"></iframe>');
+    });
+    $('#tringjollyhd, #tringjollyhd-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/tringjollyhd.html" allowfullscreen="1"></iframe>');
+    });
+    $('#tringaction, #tringaction-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/tringaction.html" allowfullscreen="1"></iframe>');
+    });
+    $('#thd, #thd-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/thd.html" allowfullscreen="1"></iframe>');
+    });
+    $('#tring3plus, #tring3plus-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/tring3plus.html" allowfullscreen="1"></iframe>');
+    });
+    $('#tringhistori, #tringhistori-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/tringhistori.html" allowfullscreen="1"></iframe>');
+    });
+    $('#tringplanet, #tringplanet-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/tringplanet.html" allowfullscreen="1"></iframe>');
+    });
+    $('#tringshqip, #tringshqip-m').click(function(){
+                $('#player').html('<iframe class="embed-responsive-item" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" src="channels/tringshqip.html" allowfullscreen="1"></iframe>');
+    });
+});
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
